d0624868a555b34d4a7a7f48ba3eb41213de6067
sohail52250 <it03346543200@gmail.com>
2026-07-22 18:16:08 +0500

    Connect Vercel API routes

--- api/index.js
+++ api/index.js
@@ -1,3 +1,22 @@
-const app = require("../server");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/api/status",(req,res)=>{
+  res.json({
+    name:"Learn & Earn Hub",
+    status:"Running",
+    database:"Supabase"
+  });
+});
+
+
+app.use("/api/users", require("./users"));
+app.use("/api/ads", require("./ads"));
+app.use("/api/courses", require("./courses"));
+
 
 module.exports = app;
