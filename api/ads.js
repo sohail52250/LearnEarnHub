28387ce2e6b9eb97e65a8f44bf3d264c0c6f7ec8
sohail52250 <it03346543200@gmail.com>
2026-07-22 18:30:38 +0500

    Add marketplace ad posting

--- api/ads.js
+++ api/ads.js
@@ -2,14 +2,62 @@ const db=require("../database");
 
 module.exports=async(req,res)=>{
 
+
+if(req.method==="GET"){
+
 const {data,error}=await db
 .from("ads")
 .select("*")
 .order("created_at",{ascending:false});
 
-res.json({
+return res.json({
 data,
 error
 });
+
+}
+
+
+
+if(req.method==="POST"){
+
+const {
+user_id,
+title,
+description,
+category,
+contact,
+location
+}=req.body;
+
+
+const {data,error}=await db
+.from("ads")
+.insert([
+{
+user_id,
+title,
+description,
+category,
+contact,
+location
+}
+])
+.select();
+
+
+return res.json({
+success:!error,
+data,
+error
+});
+
+}
+
+
+res.json({
+message:"Use GET or POST"
+});
+
 
 };
