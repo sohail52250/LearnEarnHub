9822cda73aaa7b5fad5d48c2850ff1987c09a2ad
sohail52250 <it03346543200@gmail.com>
2026-07-22 18:17:54 +0500

    Add user authentication

---
+++ api/auth.js
@@ -0,0 +1,91 @@
+const db = require("../database");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+module.exports = async(req,res)=>{
+
+if(req.method==="POST"){
+
+const {action,name,email,password,phone}=req.body;
+
+
+if(action==="register"){
+
+const hash = await bcrypt.hash(password,10);
+
+
+const {data,error}=await db
+.from("users")
+.insert([{
+name,
+email,
+password:hash,
+phone
+}])
+.select();
+
+
+return res.json({
+success:!error,
+data,
+error
+});
+
+}
+
+
+
+if(action==="login"){
+
+const {data,error}=await db
+.from("users")
+.select("*")
+.eq("email",email)
+.single();
+
+
+if(error){
+return res.json({
+success:false,
+message:"User not found"
+});
+}
+
+
+const ok = await bcrypt.compare(
+password,
+data.password
+);
+
+
+if(!ok){
+return res.json({
+success:false,
+message:"Wrong password"
+});
+}
+
+
+const token = jwt.sign(
+{id:data.id},
+"learnEarnSecret"
+);
+
+
+return res.json({
+success:true,
+token,
+user:data
+});
+
+
+}
+
+}
+
+res.json({
+message:"Use POST"
+});
+
+};
