5f16a4e27c37b5dba99a034b1de175f5e6e20ff6
sohail52250 <it03346543200@gmail.com>
2026-07-27 23:47:32 +0500

    Platform Core V2 dashboard notifications messaging

--- api/messages.js
+++ api/messages.js
@@ -3,15 +3,28 @@ const db=require("../database");
 module.exports=async(req,res)=>{
 
 if(req.method==="GET"){
+
 const user_id=req.query.user_id;
 
 const {data,error}=await db
-.from("notifications")
-.select("*")
-.eq("user_id",user_id)
-.order("created_at",{ascending:false});
+.from("messages")
+.select("*");
 
 return res.json({success:true,data,error});
+}
+
+if(req.method==="POST"){
+
+const {data,error}=await db
+.from("messages")
+.insert([req.body])
+.select();
+
+return res.json({
+success:!error,
+data,
+error
+});
 }
 
 res.status(405).json({error:"Method not allowed"});
