ceebd5840960b754e4e91003522baeeb9c0f86da
sohail52250 <it03346543200@gmail.com>
2026-07-28 20:09:40 +0500

    Fix main website static serving

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
   res.json({
