a094616c6bd8a3b9f29befff3f59fd98153b2549
sohail52250 <it03346543200@gmail.com>
2026-08-01 05:02:39 +0500

    Add course lessons API endpoint

--- routes/courses.js
+++ routes/courses.js
@@ -36,3 +36,24 @@ res.json(data);
 
 
 module.exports=router;
+
+
+// Course lessons API
+router.get("/lessons/:course_id", async(req,res)=>{
+
+    const courseId = req.params.course_id;
+
+    const {data,error}=await db
+        .from("course_lessons")
+        .select("*")
+        .eq("course_id", courseId)
+        .order("lesson_order",{ascending:true});
+
+    if(error){
+        return res.status(400).json(error);
+    }
+
+    res.json(data);
+
+});
+
