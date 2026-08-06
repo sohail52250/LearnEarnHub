b1efacaa4d9b445b6f4dc4a7d1aa5c59897b8af3
sohail52250 <it03346543200@gmail.com>
2026-08-06 21:44:25 +0500

    Remove unnecessary archive files

--- api_disabled/status.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req,res){
- res.status(200).json({
-  status:"online",
-  platform:"LearnEarnHub",
-  version:"refined",
-  languages:["en","ur"],
-  timestamp:new Date().toISOString()
- });
-}
