0aad2a768748e207a1bdca888fd181e08e50d3f0
sohail52250 <it03346543200@gmail.com>
2026-08-07 05:26:34 +0500

    Fix Supabase anon key config

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-﻿window.GOOGLE_CLIENT_ID = "1005859142498-nds3loqd4s1rgn9jvl6c2ibkhidugm6m.apps.googleusercontent.com";
-window.SUPABASE_URL = "https://srarnaqyoiqotdntzsyc.supabase.co";
+﻿window.SUPABASE_URL = "https://srarnaqyoiqotdntzsyc.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyYXJuYXF5b2lxb3RkbnR6c3ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4ODc0MjgsImV4cCI6MjEwMDQ2MzQyOH0.e8SJDfYz6jLRm_aoy5poUOYApw0OJF3SFKNKeYf4O7k";
